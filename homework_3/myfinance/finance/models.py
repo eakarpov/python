6f80c3a0b4d5f41dfc85a52ed79038b50faafd26
George <eakarpov@yandex.ru>
2016-10-17 22:55:58 +0300

    Homework 3.1
    Added connection with the database and added a possibility to add new
    charges from admin page.

--- homework_3/myfinance/finance/models.py
+++ homework_3/myfinance/finance/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Charge(models.Model):
+    CURRENCY_LIST = (
+        ('rub', 'Rubles'),
+        ('usd', 'Dollars'),
+        ('eu', 'Euros')
+    )
+    date = models.DateField()
+    amount = models.DecimalField(
+        max_digits=7,
+        decimal_places=2
+    )
+    currency = models.CharField(
+        max_length=3,
+        choices=CURRENCY_LIST,
+        default='rub'
+    )
+
+    def __str__(self):
+        return "" + str(self.date) + " " + str(self.amount) + " " + str(self.currency)
--- homework_3/myfinance/finance/views.py
+++ homework_3/myfinance/finance/views.py
@@ -2,19 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+from .models import Charge
+
 
 # Create your views here.
 def index(request):
     template = loader.get_template('index.html')
     context = RequestContext(request, {
-
+        "title": "Main page"
     })
     return HttpResponse(template.render(context))
 
 
 def charges(request):
+    variables = Charge.objects.all()
+    choices = dict(Charge.CURRENCY_LIST)
+    for var in variables:
+        var.currency = choices.get(var.currency)
     template = loader.get_template('charges.html')
     context = RequestContext(request, {
-
+        "vars": variables,
+        "title": "Account statement"
     })
     return HttpResponse(template.render(context))
